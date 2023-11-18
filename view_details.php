e57e66546d4c3b72d198d731b85458906bfd73df
Sourav <samiul.sourav00@gmail.com>
2023-11-18 22:44:53 +0600

    style updated

--- view_details.php
+++ view_details.php
@@ -9,12 +9,39 @@ include 'connect.php';
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>View All Message</title>
+    <style>
+        body {
+            height: 100vh;
+            padding: 2% 5%;
+            background: linear-gradient(135deg, #5899E2, #FFFFFF);
+        }
+
+        .title {
+            font-size: 25px;
+            font-weight: 500;
+            position: relative;
+            margin-bottom: 5px;
+
+        }
+
+        .title::before {
+            content: "";
+            position: absolute;
+            left: 0;
+            bottom: 0;
+            height: 3px;
+            width: 130px;
+            border-radius: 5px;
+            background: linear-gradient(135deg, #71b7e6, #9b59b6);
+        }
+    </style>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
 </head>
 
 <body>
-    <table class="table table-hover text center">
-        <thead class="table-dark">
+<div class="title">All Messages</div>
+    <table class="table text center table-info table-striped">
+        <thead class="">
             <tr>
                 <th scope="col">Name</th>
                 <th scope="col">Email</th>
@@ -35,9 +62,12 @@ include 'connect.php';
                     <td><?php echo $row['PhoneNumber']; ?></td>
                     <td><?php echo $row['Message']; ?></td>
                 </tr>
+
             <?php
             }
+
             ?>
+
         </tbody>
     </table>
 </body>
